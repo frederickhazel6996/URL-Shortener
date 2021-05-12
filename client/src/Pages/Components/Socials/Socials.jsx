04822217eb42a1306aeb41d3064710e140155bf5
Frederick Hazel <frederick.d9696@gmail.com>
2021-05-12 16:07:51 +0000

    changes

--- client/src/Pages/Components/Socials/Socials.jsx
+++ client/src/Pages/Components/Socials/Socials.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.scss';
 
 export default function Socials() {
   return (
@@ -7,7 +8,7 @@ export default function Socials() {
         <a
           target="_blank"
           rel="noreferrer"
-          className="btn btn-outline btn-social  mx-1"
+          className="btn socials__outline btn-social  mx-1"
           href="https://github.com/frederickhazel6996/URL-Shortener"
         >
           <i className="fab fa-fw fa-github doctor-icon"></i>
@@ -15,7 +16,7 @@ export default function Socials() {
         <a
           target="_blank"
           rel="noreferrer"
-          className="btn btn-outline btn-social  mx-1"
+          className="btn socials__outline btn-social  mx-1"
           href="https://twitter.com/meister_kwame"
         >
           <i className="fab fa-fw fa-twitter doctor-icon"></i>
